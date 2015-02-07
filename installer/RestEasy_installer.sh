8f55c44203873d30248e3da1fe7a4f560cd0bfd6
Adam Yost <swimmadude66@gmail.com>
2015-02-07 15:05:35 -0500

    Make forever install globally

--- installer/RestEasy_installer.sh
+++ installer/RestEasy_installer.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-cp ./files/sqlite3 /usr/bin/
+cp ./files/sqlite3 /usr/local/bin/
 
 mkdir -p /opt/RestEasy/
 sqlite3 /opt/RestEasy/RestEasy.db < ./files/init.sql
 
+tar -zxvf RestEasy.tar.gz -C /opt/RestEasy
 mkdir -p /opt/RestEasy/logs
 
-tar -zxvf RestEasy.tar.gz -C /opt/RestEasy
 cd /opt/RestEasy
 npm install
+
+ln -s /usr/local/bin/forever /usr/local/lib/node_modules/forever/bin/forever
 
 systemctl disable edison_config.service
 systemctl stop    edison_config.service
 
 sh /opt/RestEasy/startserver.sh
-
