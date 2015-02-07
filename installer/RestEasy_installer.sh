e97bcd13a04431acc450cb3c03879e75d187f624
Adam Yost <swimmadude66@gmail.com>
2015-02-07 15:00:21 -0500

    edison specific changes

--- installer/RestEasy_installer.sh
+++ installer/RestEasy_installer.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-sudo cp ./files/sqlite3 /usr/bin/
+cp ./files/sqlite3 /usr/bin/
+
 mkdir -p /opt/RestEasy/
-/usr/bin/sqlite3 /opt/RestEasy/RestEasy.db < ./files/init.sql
+sqlite3 /opt/RestEasy/RestEasy.db < ./files/init.sql
 
 mkdir -p /opt/RestEasy/logs
+
+tar -zxvf RestEasy.tar.gz -C /opt/RestEasy
+cd /opt/RestEasy
+npm install
+
+systemctl disable edison_config.service
+systemctl stop    edison_config.service
+
+sh /opt/RestEasy/startserver.sh
+
