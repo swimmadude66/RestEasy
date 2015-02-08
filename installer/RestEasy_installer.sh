407bada9dc8cab38a0cdd898db6460021f951eb8
Adam Yost <swimmadude66@gmail.com>
2015-02-08 14:10:28 -0500

    update installer with new file locations and start script

--- installer/RestEasy_installer.sh
+++ installer/RestEasy_installer.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-cp ./files/sqlite3 /usr/local/bin/
-
+cp ./sqlite3 /usr/local/bin/
 mkdir -p /opt/RestEasy/
-sqlite3 /opt/RestEasy/RestEasy.db < ./files/init.sql
+sqlite3 /opt/RestEasy/RestEasy.db < ./init.sql
+sh pin_init.sh
 
-tar -zxvf RestEasy.tar.gz -C /opt/RestEasy
+tar -xvf RestEasy.tar -C /opt/RestEasy
 mkdir -p /opt/RestEasy/logs
 
 cd /opt/RestEasy
 npm install
-
 ln -s /usr/local/bin/forever /usr/local/lib/node_modules/forever/bin/forever
 
 systemctl disable edison_config.service
