b4bd5feee6392e3481d707de3d352cee23702332
Adam Yost <swimmadude66@gmail.com>
2015-02-08 23:20:36 -0500

    implement the get_all, get_one, and set pins methods

--- routes/pins.js
+++ routes/pins.js
@@ -1,12 +1,26 @@
 var express = require('express');
 var rest = require('restler');
 var crypto = require('crypto');
+var childProcess = require('child_process');
 var router = express.Router();
 
 
+router.get('/api/pins/', function(req, res) {
+    childProcess.exec('python  /opt/RestEasy/python/get_pin.py -a', function(err, stdout, stderr){
+            if(err){
+                console.log(err);
+                return res.send({success: false, error: err});
+            }
+            else{
+                var result = JSON.parse(stdout);
+                return res.send(result);
+            }
+    });
+});
+
 router.get('/api/pins/:id', function(req, res) {
     var pin_id  = req.params.id;
-    child_process.exec('python  /opt/RestEasy/get_pin.py ' + pin_id.toString(), function(err, stdout, stderr){
+    childProcess.exec('python  /opt/RestEasy/python/get_pin.py ' + pin_id.toString(), function(err, stdout, stderr){
             if(err){
                 console.log(err);
                 return res.send({success: false, error: err});
@@ -19,9 +33,19 @@ router.get('/api/pins/:id', function(req, res) {
 });
 
 
-router.post('/api/pins/:id', function(req, res) {
-    //create the user here
-    h = req.headers;
+router.post('/api/pins/', function(req, res) {
+    b = req.body;
+
+    childProcess.exec('python  /opt/RestEasy/python/set_pin.py \'' + JSON.stringify(b)+'\'', function(err, stdout, stderr){
+            if(err){
+                console.log(err);
+                return res.send({success: false, error: err});
+            }
+            else{
+                var result = JSON.parse(stdout);
+                return res.send(result);
+            }
+    });
 });
 
 module.exports = router;
